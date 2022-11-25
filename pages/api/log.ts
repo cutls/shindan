39385c586ae8667ec1df060838e9aadf1ad627a0
cutls <web-pro@cutls.com>
2022-11-25 23:39:50 +0900

    log publish

--- pages/api/log.ts
+++ pages/api/log.ts
@@ -1,10 +1,16 @@
 import * as db from '../../utils/db'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import dayjs from 'dayjs'
+import { ISelected } from '../../interfaces/common'
+import { v4 as uuid } from 'uuid'
 
 interface IParam {
     id: string
-    selection: number[]
+    selection: ISelected[]
+}
+interface ILog {
+    createdAt: number
+    selection: ISelected[]
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<{} | Error>) {
@@ -12,7 +18,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         const request: IParam = JSON.parse(req.body)
         const { id, selection } = request
         const createdAt = dayjs().unix()
-        await db.put('logs', id, { selection, createdAt })
+        const got = await db.get<ILog[]>('logs', id)
+        const uid = uuid()
+        if (got) {
+            const newGot = JSON.parse(JSON.stringify(got))
+            newGot[uid] = { selection, createdAt }
+            await db.update('logs', id, got)
+        } else {
+            await db.put('logs', id, { [uid]: { selection, createdAt } })
+        }
         return res.status(200).json({} || { error: true, message: 'unknown error' })
     } catch (e: any) {
         if (e.length > 1) return res.status(e[0]).json(e[1])
